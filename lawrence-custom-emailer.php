66af23859f36536fa3d9827949011590b7e7e111
trifork <rwilliams17@lawrenceville.org>
2015-07-23 01:25:38 -0400

    added stylesheet

--- lawrence-custom-emailer.php
+++ lawrence-custom-emailer.php
@@ -9,52 +9,75 @@ Author: Ricky Williams
 // prevent direct access to plugin files (cause why not) 
 defined( 'ABSPATH' ) or die( 'Illegal access error!' ); 
 
+// register the lawrcustemail_setup_scripts() function to get the CSS working
+add_action( 'wp_enqueue_scripts', 'lawrcustemail_setup_scripts' );
+
+/**
+ * Add all stylesheets/scripts to the plugin 
+ *
+ * @since 1.0.0
+ *
+ */
+function lawrcustemail_setup_scripts()
+{
+	wp_enqueue_style( 'lawrcustemail_stylesheet', plugins_url('lawrence-custom-emailer.css', __FILE__) );
+}
+
+/**
+ * Display posts to select in a drop-down menu 
+ * 
+ * 
+ * @since 1.0.0
+ * 
+ * @see get_posts()
+ */
+function lawrcustemail_create_post_select() {
+    echo '<select>';
+    
+    // get an array of all WP_Post objects and sort them (most recent articles at the top of the menu, etc.)
+	$get_posts_args = array(
+		'posts_per_page' => -1,
+		'orderby' => 'date',
+		'order' => 'DESC',
+	);
+	
+    $all_posts = get_posts( $get_posts_args );
+    
+    // show the post titles via a select tag; if no posts could be found, then something is really wrong
+    if ( $all_posts ) {
+    	foreach ( $all_posts as $post ) {
+    		echo '<option value="' . $post->ID . '">' . $post->post_title . '</option>';
+    	}
+    } else {
+    	die( 'No posts could be found!' );
+    }
+    
+    echo '</select>';
+}
+
 // register the lawremail_add_admin_menu() function to create a new dashboard menu item
-add_action( 'admin_menu', 'lawremail_add_admin_menu' );
+add_action( 'admin_menu', 'lawrcustemail_add_admin_menu' );
 
 /** 
  * Add an admin dashboard menu to access the email customizer and emailer 
- * 
- * Description?
  * 
  * @since 1.0.0
  * 
  * @see add_dashboard_page()
  */ 
-function lawremail_add_admin_menu() {
+function lawrcustemail_add_admin_menu() {
     add_menu_page( 'Lawrence Custom Email', 'Custom Emailer', 'manage_options', 'lawrcustemail', 'lawremail_write_admin_menu' );
 }
 
 /** 
  * Write the basic html of the plugin's dashboard menu option
  * 
- * Description?
- * 
  * @since 1.0.0
  * 
  * @see?
  */ 
-function lawremail_write_admin_menu() {
-    // display possible posts to be attached in a drop-down menu 
-    echo '<select>';
-    
-	$get_posts_args = array(
-		'orderby' => 'date',
-		'order' => 'DESC',
-		'post_type' => 'post',
-		'post_status' => 'publish'
-	);
-
-    $all_posts = get_posts( $get_posts_args );
-    
-    if ( $all_posts ) {
-    	foreach ( $all_posts as $post ) {
-    		setup_postdata( $post );
-    		echo '<option value="' . the_ID() . '">' . the_title( '', '', false ) . '</option>';
-    	}
-    }
-    
-    echo '</select>';
+function lawrcustemail_write_admin_menu() {
+	lawrcustemail_create_post_select();
 }
  
 ?>
