a43aa9e4aa501ce188e957626bdc85bcc499ba15
trifork3 <rwilliams17@lawrenceville.org>
2015-07-27 16:16:24 -0400

    moved project to new computer

--- lawrence-custom-emailer.php
+++ lawrence-custom-emailer.php
@@ -5,79 +5,64 @@ Description: Plugin to create a custom email format for Lawrence articles that c
 Version: 1.0
 Author: Ricky Williams
 */
+
+define( "LWC__PLUGIN_DIR", plugin_dir_path(__FILE__) );
  
-// prevent direct access to plugin files (cause why not) 
+// prevent direct access to plugin file
 defined( 'ABSPATH' ) or die( 'Illegal access error!' ); 
 
-// register the lawrcustemail_setup_scripts() function to get the CSS working
+// include related PHP files (menus.php, etc.)
+require_once( LWC__PLUGIN_DIR . "menus.php" );
+require_once( LWC__PLUGIN_DIR . "formats.php" );
+require_once( LWC__PLUGIN_DIR . "groups.php" );
+
+/** 
+ * Add all stylesheets/scripts to the plugin.
+ */
+function lawrcustemail_setup_scripts() {
+	wp_enqueue_style( 'lawrcustemail_stylesheet', plugins_url('lawrence-custom-emailer.css', __FILE__) );
+}
 add_action( 'wp_enqueue_scripts', 'lawrcustemail_setup_scripts' );
 
 /**
- * Add all stylesheets/scripts to the plugin 
- *
- * @since 1.0.0
- *
+ * Allow wp_mail() to send HTML emails (instead of plaintext emails) so formatting can be done.
  */
-function lawrcustemail_setup_scripts()
-{
-	wp_enqueue_style( 'lawrcustemail_stylesheet', plugins_url('lawrence-custom-emailer.css', __FILE__) );
+function lawrcustemail_allow_html_mail() {
+    return "text/html";
+}
+add_filter( 'wp_mail_content_type', 'lawrcustemail_allow_html_mail' );
+
+/**
+ * Create the document formatting editor. 
+ */
+function lawrcustemail_create_format_editor() {
 }
 
 /**
- * Display posts to select in a drop-down menu 
- * 
- * 
- * @since 1.0.0
- * 
- * @see get_posts()
+ * Create the email group formatting editor.
  */
-function lawrcustemail_create_post_select() {
-    echo '<select>';
-    
-    // get an array of all WP_Post objects and sort them (most recent articles at the top of the menu, etc.)
-	$get_posts_args = array(
-		'posts_per_page' => -1,
-		'orderby' => 'date',
-		'order' => 'DESC',
-	);
-	
-    $all_posts = get_posts( $get_posts_args );
-    
-    // show the post titles via a select tag; if no posts could be found, then something is really wrong
-    if ( $all_posts ) {
-    	foreach ( $all_posts as $post ) {
-    		echo '<option value="' . $post->ID . '">' . $post->post_title . '</option>';
-    	}
-    } else {
-    	die( 'No posts could be found!' );
-    }
-    
-    echo '</select>';
+function lawrcustemail_create_email_editor() {
+    // first retrieve already created email files and display them in a drop-down menu
+    echo '<div id="email_select">';
 }
 
-// register the lawremail_add_admin_menu() function to create a new dashboard menu item
+/** 
+ * Add an admin dashboard menu to access the email customizer and emailer.
+ */ 
+function lawrcustemail_add_admin_menu() {
+    add_menu_page( 'Lawrence Custom Email', 'Custom Emailer', 'manage_options', 'lawrcustemail', 'lawrcustemail_write_admin_menu' );
+}
 add_action( 'admin_menu', 'lawrcustemail_add_admin_menu' );
 
 /** 
- * Add an admin dashboard menu to access the email customizer and emailer 
- * 
- * @since 1.0.0
- * 
- * @see add_dashboard_page()
- */ 
-function lawrcustemail_add_admin_menu() {
-    add_menu_page( 'Lawrence Custom Email', 'Custom Emailer', 'manage_options', 'lawrcustemail', 'lawremail_write_admin_menu' );
-}
-
-/** 
- * Write the basic html of the plugin's dashboard menu option
- * 
- * @since 1.0.0
- * 
- * @see?
+ * Write the basic html of the plugin's dashboard menu option.
  */ 
 function lawrcustemail_write_admin_menu() {
-	lawrcustemail_create_post_select();
+    // print out the drop-down menu from which the post to be emailed is selected
+    lawrcustemail_create_post_select();
+    
+    // create the editor to select and edit email formatting files 
+    lawrcustemail_create_format_editor();
 }
  
 ?>
